c58a25deefe715efd0d1c0a1ef2086e6a3146981
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 14:36:10 +0200

    adding dynamic color to pins with function

--- map1.py
+++ map1.py
@@ -5,13 +5,21 @@ data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
+
+def color(el):
+    if el < 1000:
+        return 'green'
+    elif 1000 <= el < 3000:
+        return 'orange'
+    else:
+        return 'red'
 # print(lat)
 map= folium.Map(location=[38.58,-99.09], zoom_start=5, tiles='Stamen Terrain')
 
 fg= folium.FeatureGroup(name='My Map')
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= 'red' if el > 2000 else 'green' )))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= color(el) )))
 
 map.add_child(fg)
 map.save('Map1.html')
