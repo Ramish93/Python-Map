137ee91493bd2531a7f0502da12ba89a647c8d69
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 14:27:18 +0200

    adding dynamic color to pins with python ternary operator

--- map1.py
+++ map1.py
@@ -4,12 +4,14 @@ import pandas
 data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
-print(lat)
+elev = list(data['ELEV'])
+# print(lat)
 map= folium.Map(location=[38.58,-99.09], zoom_start=5, tiles='Stamen Terrain')
+
 fg= folium.FeatureGroup(name='My Map')
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup='hi there im a volcano', icon=folium.Icon(color='red')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= 'red' if el > 2000 else 'green' )))
 
 map.add_child(fg)
 map.save('Map1.html')
