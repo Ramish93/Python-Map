3e850d09bcc1419683760f77483038f13496c871
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 15:41:29 +0200

    layers added for population and volcanos seprately, project complete

--- map1.py
+++ map1.py
@@ -16,12 +16,19 @@ def color(el):
 # print(lat)
 map= folium.Map(location=[38.58,-99.09], zoom_start=5, tiles='Stamen Terrain')
 
-fg= folium.FeatureGroup(name='My Map')
+fgv= folium.FeatureGroup(name='Volcanos')
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= color(el) )))
+    fgv.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= color(el) )))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r',encoding='UTF-8-sig').read(), style_function=lambda x:{'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+fgp= folium.FeatureGroup(name='Population')
+
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r',encoding='UTF-8-sig').read(), style_function=lambda x:{'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save('Map1.html')
