e1190077ca2a632592636b0019aacce842529c1f
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 15:34:52 +0200

    added population styling depending on number of population

--- map1.py
+++ map1.py
@@ -21,7 +21,7 @@ fg= folium.FeatureGroup(name='My Map')
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(location=[lt, ln], popup=f'elevation of volcano is: {str(el)} m', icon=folium.Icon(color= color(el) )))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r',encoding='UTF-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r',encoding='UTF-8-sig').read(), style_function=lambda x:{'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save('Map1.html')
