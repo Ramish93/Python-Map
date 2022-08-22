c8098efa4466c898a4213c09cb9b72bc169b9d10
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 14:14:23 +0200

    added volcano.txt which contain data about volanos in US. loaded it on map with lon-latitue

--- map1.py
+++ map1.py
@@ -1,7 +1,15 @@
 import folium
+import pandas
 
-map= folium.Map(location=[31.752, 72.917], zoom_start=5, tiles='Stamen Terrain')
+data = pandas.read_csv('Volcanoes.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+print(lat)
+map= folium.Map(location=[38.58,-99.09], zoom_start=5, tiles='Stamen Terrain')
 fg= folium.FeatureGroup(name='My Map')
-fg.add_child(folium.Marker(location=[31.652, 72.817], popup='hi there', icon=folium.Icon(color='red')))
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup='hi there im a volcano', icon=folium.Icon(color='red')))
+
 map.add_child(fg)
 map.save('Map1.html')
