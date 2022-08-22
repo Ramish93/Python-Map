185c95ff1ff0b4f0a859f968306137800c75d4f4
Ramish Hassan <ramishshah93@gmail.com>
2022-08-22 11:27:14 +0200

    added marker to map

--- map1.py
+++ map1.py
@@ -1,4 +1,7 @@
 import folium
 
 map= folium.Map(location=[31.752, 72.917], zoom_start=5, tiles='Stamen Terrain')
+fg= folium.FeatureGroup(name='My Map')
+fg.add_child(folium.Marker(location=[31.652, 72.817], popup='hi there', icon=folium.Icon(color='red')))
+map.add_child(fg)
 map.save('Map1.html')
